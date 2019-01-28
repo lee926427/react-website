bbdb394e426095640e31611ef73d2e6915bd7446
lee926427 <leev126g8@gmail.com>
2019-01-28 10:34:45 +0800

    react-website

---
+++ precache-manifest.35299454faf14cb7354c1759593f44c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d19556a06fe8f61eeac",
+    "url": "/static/css/main.a42adfc6.chunk.css"
+  },
+  {
+    "revision": "3d19556a06fe8f61eeac",
+    "url": "/static/js/main.3d19556a.chunk.js"
+  },
+  {
+    "revision": "dafcb6a954e4321cc7c0",
+    "url": "/static/css/1.13ae926e.chunk.css"
+  },
+  {
+    "revision": "dafcb6a954e4321cc7c0",
+    "url": "/static/js/1.dafcb6a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a4d6c264df8489bafc79b0c92a2fa44",
+    "url": "/index.html"
+  }
+];
